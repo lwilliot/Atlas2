165d7c9390e6e72c4e612130be725d4a241e2fd3
lwilliot <lucas.williot@student.uclouvain.be>
2022-12-30 17:12:33 +0100

    remove filter and normalize on destination ranking

--- rank.py
+++ rank.py
@@ -1,43 +1,37 @@
 import json
 import sys
 
+
+
 path=sys.argv[1]
-threshhold=1.0
 f=open(path)
 data=json.load(f)
 ases=list(data.keys())
 nb_destination=len(data[ases[0]])
 remaining_destination=len(data[ases[0]])
-means=[]
 data_by_destination=[]
 leaderboard={}
 
-#clean the data compute means and format by destination
+
+
+#clean the data by removing destinations that are not pinged by all AS. 
+# And format data by destination instead of by AS to make the ranking easier to compute.
 def clear():
     global means
     global remaining_destination
     global data_by_destination
 
-    means=[]
     data_by_destination=[]
     to_clean=[]
+    #add index of invalid destination in to_clean and build data_by_destination
     for dest in range(nb_destination):
-        count=0
-        count_mean=0
-        acc=0.0
         data_by_destination.append({})
         for AS in ases:
             data_by_destination[dest][AS]=data[AS][dest]
             if data[AS][dest]==-1:
-                count+=1
-            else:
-                count_mean +=1
-                acc+=data[AS][dest]
-        
-        if(len(ases)-count>=(int)(len(ases)*threshhold)):
-            means.append(acc/count_mean)
-        else:
-            to_clean.append(dest)
+                to_clean.append(dest)
+                break
+    #remove invalid destinations
     count=0
     for ind in to_clean:
         del(data_by_destination[ind-count])
@@ -46,7 +40,48 @@ def clear():
 
 
 
-while(True):
+
+
+
+clear()
+#Sort the AS for each destination and normalize the RTT value
+for i in range (len(data_by_destination)):
+    data_by_destination[i]=dict(sorted(data_by_destination[i].items(), key=lambda item: item[1]))
+    liste =(list(data_by_destination[i].values()))
+    min=float( liste[0])
+    max=float(liste[len(liste)-1])
+    for AS in data_by_destination[i]:
+        data_by_destination[i].update({AS:1-((data_by_destination[i][AS]-min)/(max-min))})
+
+
+
+#build global leaderboard
+for AS in ases:
+    leaderboard[AS]=0
+
+#Add value of each ranking to leaderboard and compute the mean.
+count=1
+for dest in data_by_destination:
+    point=len(ases)
+    for AS in dest:
+        multiplier=0
+        leaderboard.update({AS:leaderboard[AS]+dest[AS]})
+        if(count==remaining_destination):
+            leaderboard.update({AS:round((leaderboard[AS]/(remaining_destination)),3)}) 
+    count+=1
+leaderboard=dict(sorted(leaderboard.items(), key=lambda item: item[1],reverse=True))
+
+
+
+print("\nRESULTS:")
+for item in leaderboard.items():
+    print(item)
+
+
+
+
+
+""" while(True):
     clear()
     user_input=input("Rank will be calculated from pings on {} differents targets. Do you wanna try to increase the number of targets? (It could lower the quality of the data) (y/n): ".format(remaining_destination))
     if user_input =="y":
@@ -59,11 +94,10 @@ while(True):
     elif user_input=="n":
         break
     else:
-        print("Bad response.Please type y or n")
+        print("Bad response.Please type y or n") """
 
 
-#rank by destination and update remaining -1
-for i in range (len(data_by_destination)):
+""" for i in range (len(data_by_destination)):
     data_by_destination[i]=dict(sorted(data_by_destination[i].items(), key=lambda item: item[1]))
     replace=False
     for neg in data_by_destination[i]:
@@ -73,29 +107,4 @@ for i in range (len(data_by_destination)):
         else:
             break
     if(replace):
-        data_by_destination[i]=dict(sorted(data_by_destination[i].items(), key=lambda item: item[1]))
-
-#build global leaderboard
-for AS in ases:
-    leaderboard[AS]=0
-
-count=1
-for dest in data_by_destination:
-    point=len(ases)
-    for AS in dest:
-        leaderboard.update({AS:leaderboard[AS]+point})
-        point-=1
-        if(count==remaining_destination):
-            leaderboard.update({AS:round((leaderboard[AS]/(remaining_destination*len(ases))),2)})
-    count+=1
-
-
-
-
-leaderboard=dict(sorted(leaderboard.items(), key=lambda item: item[1],reverse=True))
-
-
-
-print("\nRESULTS:")
-for item in leaderboard.items():
-    print(item)
+        data_by_destination[i]=dict(sorted(data_by_destination[i].items(), key=lambda item: item[1])) """
